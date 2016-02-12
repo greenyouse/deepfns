b470c57a6470c187df14529f29e997ddb6c77238
greenyouse <greenyouse@hotmail.com>
2016-02-11 21:59:00 -0600

    Bump to v0.1.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.greenyouse/deepfns "0.1.3"
+(defproject com.greenyouse/deepfns "0.1.4"
   :description "Deeply nested fmap, fapply, and more!"
   :url "https://github.com/greenyouse/deepfns"
   :license {:name "Eclipse Public License"
